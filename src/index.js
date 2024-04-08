d598fc5ac7ea034484cdc77837f5d0d30e83be05
gabriel <gabrieldosanjosdbz@gmil.com>
2024-04-08 11:49:36 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express  = require('express')
+const mongoose = require('mongoose')
+ 
+const app = express()
+app.use(express.json())
+const port = 3000
+ 
+const Film = mongoose.model('filme', {
+      title:        String,
+      description : String,
+      image_url:    String,
+      trailer_url:  String
+})
+ 
+app.listen(port, () => {
+    mongoose.connect('mongodb+srv://gabrieldosanjosdbz:BLAua0I3Dp3pR2nK@starwars-api.5cnajl8.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api')
+    console.log(`API rodando na porta ${port}`)
+})
+ 
+app.get('/', async(req, res) =>{
+    const films = await Film.find()
+    return res.send(films)
+}),
+ 
+app.post('/', async(req, res) =>{
+    const film = new Film({
+        title:        req.body.title,
+        description : req.body.description,
+        image_url:    req.body.image_url,
+        trailer_url:  req.body.trailer_url
+    })
+ 
+    await film.save()
+    return res.send(film)
+}),
+ 
+app.delete('/:id', async(req, res) =>{
+    const film = await Film.findByIdAndDelete(req.params.id)
+    return res.send(film)
+})
+ 
+app.put('/:id', async(req, res) => {
+    const film = await Film.findByIdAndUpdate(req.params.id, {
+        title:        req.body.title,
+        description : req.body.description,
+        image_url:    req.body.image_url,
+        trailer_url:  req.body.trailer_url
+    }, {
+        new: true
+    })
+    return res.send(film)
+})
