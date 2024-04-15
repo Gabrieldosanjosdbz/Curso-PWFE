3613f0c94fb61f5abbbacd3a5de55a14a6b7720a
gabriel <gabrieldosanjosdbz@gmil.com>
2024-04-15 11:59:39 -0300

    fix2

--- src/pages/FIlme/index.js
+++ src/pages/FIlme/index.js
@@ -1,53 +1,50 @@
-import { useState, useEffect, useParams, useHistory } from "react";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { toast } from 'react-toastify';
 import api from '../../services/api';
 import './estilo.css';
 
 function Filme(){
-    const { id } = useParams();
-    const history = useHistory();
 
-    const [filme, setFilme] = useState([]);
+    const { id }  = useParams(); //é um hook do React Router que permite acessar os parâmetros de rota em componentes de uma aplicação React.
 
+    const [ filme, setFilme ] = useState([]);
+    
     useEffect(() => {
         async function loadFilme(){
             const res = await api.get(`r-api/?api=filmes/${id}`);
-
-            if(res.data.length === 0){ //.data é onde os dados retornados pela resposta da solicitação são armazenados.
-                history.replace('/'); //é usado para substituir a entrada atual no histórico de navegação por uma nova entrada sem adicionr ao historico de navegação.
-
-            };
             setFilme(res.data);
         };
         loadFilme();
-    }, []);
+    });
 
     function salvarFilme(){
         const minhaLista = localStorage.getItem('filmes');
         let filmesSalvos = JSON.parse(minhaLista) || [];
 
         //se tiver algum filme salvo com esse id precisa ignorar
-        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+        const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id);
 
         if(hasFilme){
             toast.error('Você já salvou este filme!');
-            return;
+            return
         }
-        filmesSalvos.push(filme)
-        localStorage.setItem("filmes", JSON.stringify(filmesSalvos))
-        toast.success("O filme foi adicionado a sua lista de favoritos")
-    }
+        filmesSalvos.push(filme);
+        localStorage.setItem('filmes', JSON.stringify(filmesSalvos));
+        toast.success('Filme salvo com sucesso!');
+    };
 
-    return (
-        <div className="container">
-            <div className="filme-info">
+    return(
+        <div className='container'>
+            <div className='filme-info'>
                 <article key={filme.id}>
-                    <h2>{filme.nome}</h2>
-                    <img src={filme.foto} alt={filme.foto} />
+                    <h2>{ filme.nome }</h2>
+                    <img src={ filme.foto } alt={filme.foto} />
                     <h3>Sinopse</h3>
-                    <p>{filme.sinopse}</p>
+                    <p>{ filme.sinopse }</p>
 
-                    <div className="botoes">
+                    <div className='botoes'>
                         <button onClick={salvarFilme}>Salvar</button>
                         <button>
                             <a target='blank' 
@@ -56,11 +53,11 @@ function Filme(){
                             </a>
                         </button>
                     </div>
+
                 </article>
             </div>
         </div>
-    )
+    );
 }
-   
 
 export default Filme;
