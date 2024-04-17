d18dd43fe1a2604fc7badf1d8a805e5a384ff65b
gabriel <gabrieldosanjosdbz@gmil.com>
2024-04-17 08:28:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  // Valor do estado/variavel e função que atualiza ela 
+  const [endereco, setEndereco] = useState({});
+
+  function manipularEndereco (event) {
+
+    const cep = event.target.value
+
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8){
+      // obter o cep´
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((dados) => {
+        setEndereco(enderecoAntigo => {
+          return {
+          ...enderecoAntigo,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+          estado: dados.uf
+          }
+         })
+      })
+
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">             {/*método para alterar o valor do estado pelo input, o onChange é uma função que é chamada sempre quando esse input mudar*/}
+        <input type='number' placeholder='Digite o cep' onChange={manipularEndereco}></input>
+         <ul>
+          <li>CEP: {endereco.cep}</li>   
+          <li>Bairro: {endereco.bairro}</li>  
+          <li>Cidade: {endereco.cidade}</li>  
+          <li>Estado: {endereco.estado}</li>  
+         </ul>
       </header>
     </div>
   );
